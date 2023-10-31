08ef896e7dc96eb9402d5af74c292fe5d362b244
Ayushman Lakshkar <ayushman.lakshkar7@gmail.com>
2023-11-01 02:34:21 +0530

    updated project

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,77 +1,86 @@
-clockp = document.getElementById('clock')
-input=document.getElementById('item')
-added=document.getElementById('add')
-sno=document.getElementById('serialno')
-text=document.getElementById('text')
-timedone=document.getElementById('donetime')
-statu=document.getElementById('status')
-todelete=document.getElementById('todelete')
-timestamp=document.getElementById('timestamp')
-delu=document.getElementsByClassName('del')
-number=1
+common_clock = document.getElementById('clock')
+to_do_set = document.getElementById('to_do')
+add_button = document.getElementById('add')
+deadline_set = document.getElementById('timestamp')
+box = document.getElementsByClassName('box')[0]
+number = 0
 
-clock=()=>{
-    let fulldate=new Date()
-    clockp.innerHTML=fulldate
+clock = () => {
+    let fulldate = new Date()
+    common_clock.innerHTML = fulldate
+    setInterval(clock, 1000);
 }
-setInterval(clock,1000);
+clock()
 
 
-showtime=(timeitem)=>{
-    let fulldate=new Date(Date.parse(timestamp.value))
-    hours=fulldate.getHours()
-    minutes=fulldate.getMinutes()
-    date=fulldate.getDate()
-    month=fulldate.getMonth()
-    year=fulldate.getFullYear()
-    timeitem.innerHTML= hours+ ' : '+minutes+ " ; "+date+' / '+month+' / '+year
+set_deadline = () => {
+    let fulldate = new Date(Date.parse(deadline_set.value))
+    hours = fulldate.getHours()
+    minutes = fulldate.getMinutes()
+    date = fulldate.getDate()
+    month = fulldate.getMonth()
+    year = fulldate.getFullYear()
+    return hours + ' : ' + minutes + " &nbsp , " + date + ' / ' + month + ' / ' + year
 }
 
 
-
-add=()=>{
-    
-    //serial numbers
-    itemno=document.createElement('li')
-    itemno.innerHTML=number
-    sno.appendChild(itemno)
-    number++
-    
-    
-    //text message
-    listitem=document.createElement('li')
-    listitem.innerHTML=input.value
-    text.appendChild(listitem)
-    
-    // time on done
-    timeitem=document.createElement('li')
-    showtime(timeitem)
-    timedone.appendChild(timeitem)
-    
-    
-    //add delete btn
-    delbox=document.createElement('li')
-    delbtn=document.createElement('button')
-    delbtn.classList.add('del')
-    delbtn.innerHTML='DELETE'
-    todelete.appendChild(delbox)
-    delbox.appendChild(delbtn)
-    
+serial_rearrange = () => {
+    row = document.getElementsByClassName('row')
+    for (let i = 1; i < row.length; i++) {
+        row[i].children[0].innerHTML = i
+    }
 }
 
-deletefn = ()=>{
-   console.log('ayush')
+
+
+delete_task = (e) => {
+    e.target.parentElement.remove();
+    serial_rearrange();
+    number = number - 1
 }
-    
-    
-    
 
 
-added.addEventListener('click',add)
-for (var i = 0 ; i < delu.length; i++) {
-    console.log(delu[i])
- }
 
-    
-    
+add = () => {
+    if (to_do_set.value == '' || deadline_set.value == '') {
+        alert('Please fill all the fields')
+    }
+    else {
+        row = document.createElement('ul')
+        row.classList.add('row')
+
+        serial = document.createElement('li')
+        number = number + 1
+        serial.innerHTML = number
+        serial.classList.add('column', 'serial')
+        row.appendChild(serial)
+
+        task = document.createElement('li')
+        task.innerHTML = to_do_set.value
+        to_do_set.value = ''
+        task.classList.add('column', 'task')
+        row.appendChild(task)
+
+        deadline = document.createElement('li')
+        deadline.innerHTML = set_deadline()
+        deadline_set.value = ''
+        deadline.classList.add('column', 'deadline')
+        row.appendChild(deadline)
+
+        remove = document.createElement('button')
+        remove.innerHTML = "remove"
+        remove.classList.add('column', 'remove')
+        row.appendChild(remove)
+        box.appendChild(row)
+        remove.addEventListener('click', (e) => { delete_task(e) });
+
+    }
+
+}
+
+
+add_button.addEventListener('click', add)
+
+
+
 
